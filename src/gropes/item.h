e9250fc4218eb03c1be81f0457a4f7061d8c1a3e
Ville Tervo <vitervo@nullier.(none)>
2007-05-13 21:07:43 +0300

    [ADD FILE] Add item.g
    Add item.h

---
+++ src/gropes/item.h
@@ -0,0 +1,9 @@
+#ifndef GROPES_ITEM
+#define GROPES_ITEM
+
+#include <gropes.h>
+
+void item_clear_track(struct item_on_screen *item);
+void item_clear_track(struct item_on_screen *item);
+
+#endif /* GROPES_ITEM */
